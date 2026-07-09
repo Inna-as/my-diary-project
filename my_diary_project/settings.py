1294a22eb47688ac064ebf0d563e993f04049abb
inna <aleksandr9311737@mail.ru>
2026-07-09 14:39:20 +0300

    Stage 2: Кэш переведён на LocMemCache, убрана зависимость от внешнего Redis

--- my_diary_project/settings.py
+++ my_diary_project/settings.py
@@ -95,11 +95,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # настройка кеширования (
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake",
     }
 }
 
